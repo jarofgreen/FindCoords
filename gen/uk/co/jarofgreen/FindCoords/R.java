4709ce2dc59e2fb063c3b1271a10b8df24f4c221
James <james@jarofgreen.co.uk>
2012-01-01 11:13:04 +0000

    Fix loading layout

--- gen/uk/co/jarofgreen/FindCoords/R.java
+++ gen/uk/co/jarofgreen/FindCoords/R.java
@@ -20,27 +20,28 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int about=0x7f060015;
+        public static final int about=0x7f060016;
         public static final int about_text=0x7f060000;
-        public static final int bearing=0x7f06000c;
-        public static final int cancel=0x7f060010;
-        public static final int currentAccuracy=0x7f060009;
-        public static final int currentBearing=0x7f060008;
-        public static final int currentLat=0x7f060006;
-        public static final int currentLng=0x7f060007;
-        public static final int current_menu_item=0x7f060012;
-        public static final int desiredBearing=0x7f06000b;
-        public static final int desiredDistance=0x7f06000a;
-        public static final int desiredLat=0x7f060004;
-        public static final int desiredLng=0x7f060005;
-        public static final int entry=0x7f06000e;
-        public static final int label=0x7f06000d;
+        public static final int bearing=0x7f06000d;
+        public static final int cancel=0x7f060011;
+        public static final int comma=0x7f060003;
+        public static final int currentAccuracy=0x7f06000a;
+        public static final int currentBearing=0x7f060009;
+        public static final int currentLat=0x7f060007;
+        public static final int currentLng=0x7f060008;
+        public static final int current_menu_item=0x7f060013;
+        public static final int desiredBearing=0x7f06000c;
+        public static final int desiredDistance=0x7f06000b;
+        public static final int desiredLat=0x7f060005;
+        public static final int desiredLng=0x7f060006;
+        public static final int entry=0x7f06000f;
+        public static final int label=0x7f06000e;
         public static final int lat=0x7f060002;
-        public static final int lng=0x7f060003;
-        public static final int load_menu_item=0x7f060014;
-        public static final int map_menu_item=0x7f060011;
-        public static final int ok=0x7f06000f;
-        public static final int save_menu_item=0x7f060013;
+        public static final int lng=0x7f060004;
+        public static final int load_menu_item=0x7f060015;
+        public static final int map_menu_item=0x7f060012;
+        public static final int ok=0x7f060010;
+        public static final int save_menu_item=0x7f060014;
         public static final int title=0x7f060001;
     }
     public static final class layout {
